031e7e3a62e7f5fe04d62da3fff0dc2e35a281e5
danyadev <danyadev@mail.ru>
2020-07-04 21:46:13 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <button @click="click">Click me</button>
+
+  <KeepAlive>
+    <Foo :key="key" />
+  </KeepAlive>
+</template>
+
+<script>
+import { ref } from 'vue'
+import Foo from './Foo.vue';
+
+export default {
+  components: {
+    Foo
+  },
+
+  setup() {
+    const key = ref(0)
+    const click = () => {
+      key.value = key.value ? 0 : 1
+    }
+
+    return {
+      click,
+      key
+    }
+  }
+}
+</script>
+
+<style scoped>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+
+</style>
